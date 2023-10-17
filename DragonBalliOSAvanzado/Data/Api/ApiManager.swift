988b5a3cd610af543af79c2e2b9196dd21b0e86c
Manuel Cazalla <manuelcm23@hotmail.com>
2023-10-17 07:29:10 +0200

    Trabajo con las llamadas a la api

---
+++ DragonBalliOSAvanzado/Data/Api/ApiManager.swift
@@ -0,0 +1,60 @@
+//
+//  ApiManager.swift
+//  DragonBalliOSAvanzado
+//
+//  Created by Manuel Cazalla Colmenero on 17/10/23.
+//
+
+import Foundation
+
+// MARK: - Protocol
+
+// MARK: Class
+class ApiManager {
+    
+    enum ApiError: Error {
+        case unknow
+        case malformedUrl
+        case decodingFailed
+        case encodingFailed
+        case noData
+        case statusCode(code: Int?)
+        case noToken
+    }
+    
+    private enum ApiConfig {
+        static let apiBaseURL = "https://dragonball.keepcoding.education/api"
+    }
+    enum EndPoint {
+        static let login = "/auth/login"
+    }
+    
+    func login(user: String,
+               password: String,
+               completion: @escaping (Result<String, ApiError>) -> Void) {
+        
+        guard let url = URL(string: "\(ApiConfig.apiBaseURL)\(EndPoint.login)") else {
+            completion(.failure(.malformedUrl))
+            return
+        }
+        
+        guard let dataLogin = String(
+            format: "%@:%@",
+            user, password).data(using: .utf8)?.base64EncodedString() else {
+            completion(.failure(.encodingFailed))
+            return
+        }
+        
+        var urlRequest = URLRequest(url: url)
+        urlRequest.httpMethod = "POST"
+        urlRequest.setValue("Basic \(dataLogin)", forHTTPHeaderField: "Authorization")
+        
+        let task = URLSession.shared.dataTask(with: urlRequest) { data, response, error in
+            
+            guard error == nil else {
+                completion(.failure(.unknow))
+                return
+            }
+        }
+    }
+}
---
+++ DragonBalliOSAvanzado/Data/Local/keychainManager.swift
@@ -0,0 +1,32 @@
+//
+//  keychainManager.swift
+//  DragonBalliOSAvanzado
+//
+//  Created by Manuel Cazalla Colmenero on 16/10/23.
+//
+
+import Foundation
+import KeychainSwift
+
+// MARK: Protocol
+protocol KeychainManagerProtocol {
+    func save(token: String)
+    func getToken() -> String?
+    
+}
+// MARK: Class
+class KeychainManager: KeychainManagerProtocol  {
+    private let keychain = KeychainSwift()
+    
+    private enum ApiKey {
+        static let token = "ApiToken"
+    }
+    
+    func save(token: String) {
+        keychain.set(token, forKey: ApiKey.token)
+    }
+    
+    func getToken() -> String? {
+        keychain.get(ApiKey.token)
+    }
+}
